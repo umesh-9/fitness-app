b6d11d7f724dd91a7e51bcac1aa2fda4cb0f40e5
umesh-9 <umeshnaidu00@gmail.com>
2023-07-27 11:13:29 +0530

    add equipment component

--- src/components/SimilarExercises.jsx
+++ src/components/SimilarExercises.jsx
@@ -8,12 +8,22 @@ const SimilarExercises = ({
 }) => {
   return (
     <Box sx={{ mt: { lg: "100px", xs: "0" } }}>
-      <Typography variant="h3">
+      <Typography variant="h3" mb={5}>
         Exercises that target the same muscle group
       </Typography>
       <Stack direction="row" sx={{ p: "2", position: "relative" }}>
         {targetMuscleExercises.length ? (
           <HorizontalScrollbar data={targetMuscleExercises} />
+        ) : (
+          <Loader />
+        )}
+      </Stack>
+      <Typography variant="h3" mb={5}>
+        Exercises that use the same equipment
+      </Typography>
+      <Stack direction="row" sx={{ p: "2", position: "relative" }}>
+        {equipmentMuscleExercises.length ? (
+          <HorizontalScrollbar data={equipmentMuscleExercises} />
         ) : (
           <Loader />
         )}
